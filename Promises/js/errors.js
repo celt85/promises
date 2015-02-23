d790d639db436d9d41909b8dbfb4f4d0e7be82a8
mbalda <mbalda@gmail.com>
2015-02-24 00:19:53 +0100

    Add error handling files.

---
+++ Promises/js/errors.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    "use strict";
+
+    var BUTTON_SELECTOR = "button";
+    var FIRST_NUMBER_SELECTOR = "#firstNumber";
+    var SECOND_NUMBER_SELECTOR = "#secondNumber";
+
+    WinJS.UI.Pages.define("/pages/errors.html", {
+        ready: function (element, options) {
+
+            var firstNumber = element.querySelector(FIRST_NUMBER_SELECTOR);
+            var secondNumber = element.querySelector(SECOND_NUMBER_SELECTOR);
+
+            calculate(firstNumber, secondNumber);
+        }
+    });
+
+    function calculate(firstNumber, secondNumber) {
+        WinJS.Promise(function (complete, error, progress) {
+
+            try {
+                var result = firstNumber / secondNumber;
+                complete(result)
+            }
+            catch (err) {
+
+            }
+        });
+    }
+
+})();
